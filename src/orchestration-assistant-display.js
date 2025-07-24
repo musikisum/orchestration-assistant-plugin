c05ebf7cc55d68194708d23dec2fd7c85ca1d74c
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2025-07-25 00:18:27 +0200

    Rename table and delete unused text variable

--- src/orchestration-assistant-display.js
+++ src/orchestration-assistant-display.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Table from './table.js'; 
+import TableOfInstruments from './table-of-instruments.js'; 
 // import { useTranslation } from 'react-i18next';
 import { sectionDisplayProps } from '@educandu/educandu/ui/default-prop-types.js';
 
 export default function OrchestrationAssistantDisplay({ content }) {
 
-  const { text, from, to, width } = content;
+  const { from, to, width } = content;
 
   // const { t } = useTranslation('musikisum/educandu-plugin-orchestration-assistant');
 
   return (
     <div className="EP_Educandu_Orchestration_Assistant_Display">
       <div className={`u-horizontally-centered u-width-${width}`}>
-        <Table from={from} to={to} />
+        <TableOfInstruments from={from} to={to} />
       </div>
     </div>
   );
